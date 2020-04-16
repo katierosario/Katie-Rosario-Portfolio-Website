dae322e4a2d5e395ee8a2c039e69a8909630dc30
katierosario <61131044+katierosario@users.noreply.github.com>
2020-04-16 14:44:04 -0400

    Initial commit

---
+++ Katie Rosario Portfolio Website/assets/js/ReverseString.js
@@ -0,0 +1,21 @@
+﻿
+
+$("#tacoCat").on("click", function () {
+    var str = $("#reverse").val();
+    var reversedstr = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        reversedstr += str[i];
+    }
+    $("#reversed").html(reversedstr);
+
+    if (str == reversedstr) {
+        alert(str + " is a palidrome");
+        $("#isPalindrome").prop("checked", true);
+    } else {
+        alert(str + " is not a palidrome");
+        $("#isPalindrome").prop("checked", false);
+    }
+
+
+});
+
---
+++ Katie Rosario Portfolio Website/assets/js/SunsetHills.js
@@ -0,0 +1,40 @@
+﻿<script>
+
+
+$("#sunonbuildings").on("click", function () {
+
+    let myArray = new Array();
+    myArray.push(Number($("#building1").val()));
+    myArray.push(Number($("#building2").val()));
+    myArray.push(Number($("#building3").val()));
+    myArray.push(Number($("#building4").val()));
+    myArray.push(Number($("#building5").val()));
+    myArray.push(Number($("#building6").val()));
+
+    let tallest = myArray[0]
+
+    let newArray = new Array();
+    newArray.push(tallest);
+    for (var index = 1; index < myArray.length; index++) {
+        if (myArray[index] > tallest) {
+            tallest = myArray[index];
+            newArray.push(tallest);
+
+        }
+
+    }
+
+    $("#hillsOutput").html(`The buildings that can see the sunset are ${newArray.join(", ")}`);
+    });
+
+    $("#clearHills").on("click", function () {
+        $('#hillsInput1, ").val("");
+        $("#hillsOutput").text("");
+
+    });
+
+    $('#btnShowHillscode").on("click", function (){
+
+
+    }
+</script>
