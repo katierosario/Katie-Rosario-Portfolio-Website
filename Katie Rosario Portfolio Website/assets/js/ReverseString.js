ce271ba0d9ff0d6abe141f0edc8f48f959d44d90
katierosario <61131044+katierosario@users.noreply.github.com>
2020-05-14 15:37:51 -0400

    Updates & Adding Information

--- Katie Rosario Portfolio Website/assets/js/ReverseString.js
+++ Katie Rosario Portfolio Website/assets/js/ReverseString.js
@@ -1,26 +1,36 @@
 ﻿
 
-$("#tacoCat").on("click", function () {
+$("#buttonreverse").on("click", function () {
     var str = $("#reverse").val();
-    var reversedstr = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-        reversedstr += str[i];
-    }
-    $("#reversed").html(reversedstr);
+    if (str != '') {
+        var reversedstr = "";
+        for (var i = str.length - 1; i >= 0; i--) {
+            reversedstr += str[i];
+        }
+        $("#reversed").html(reversedstr);
 
-    if (str == reversedstr) {
+        if (str == reversedstr) {
+            swal({
+                title: "Yay!",
+                text: `${str} is a palindrome!`,
+                icon: "success",
+            });
+        } else {
+            swal({
+                title: "Nope!",
+                text: `${str} is a not palindrome.`,
+                icon: "error"
+            });
+        }
+    }
+    else {
         swal({
-            title: "Yay!",
-            text: `${str} is a palindrome!`,
-            icon: "success",
-        });
-    } else {
-        swal({
-            title: "Nope!",
-            text: `${str} is a not palindrome.`,
-            icon: "error"
+            title: "Hold Up!",
+            text: `Field cannot be blank.`,
+            icon: "warning"
         });
     }
+
 
 });
 
